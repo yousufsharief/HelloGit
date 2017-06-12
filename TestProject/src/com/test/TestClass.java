2d6bda5f819b0ff0f1b43740f0a3ef48e195f445
yousufsharief <ysharief.786@gmail.com>
2017-06-12 03:11:01 -0500

    1st checking

--- TestProject/src/com/test/TestClass.java
+++ TestProject/src/com/test/TestClass.java
@@ -4,8 +4,9 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi");
-		System.out.println("================================  ");
+		System.out.println(" ================================  ");
 		System.out.println(" bye");
+		System.out.println("****");
 
 	}
 
