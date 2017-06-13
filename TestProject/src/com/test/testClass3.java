77784ac4b72f235537a06130a8cad74216081387
yousufsharief <ysharief.786@gmail.com>
2017-06-13 17:16:55 +0530

    test jboss*2

--- TestProject/src/com/test/testClass3.java
+++ TestProject/src/com/test/testClass3.java
@@ -5,7 +5,7 @@ public class testClass3 {
 	public static void main(String[] args) {
 		System.out.println("***********************");
 		System.out.println(" ================================  ");
-		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%**");
 
 	}
 
