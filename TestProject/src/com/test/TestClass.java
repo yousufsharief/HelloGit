49e3212c849cbce5623ffbb46b31730ce45b0300
yousufsharief <ysharief.786@gmail.com>
2017-06-15 15:48:56 +0530

    dddd

--- TestProject/src/com/test/TestClass.java
+++ TestProject/src/com/test/TestClass.java
@@ -3,12 +3,33 @@ package com.test;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hi");
-		System.out.println(" ================================  ");
-		System.out.println(" bye");
-		System.out.println("****");
-		System.out.println("&&&&   *****");
-		System.out.println("&&&&   *****" + "1234567890vasd  "); 
+		System.out.println("hi");
+		 short a=10;
+		 short b=10;
+		// a=  (a+b);
+		// a= (short) (a+b);
+		 a+=b;
+		 System.out.println(a);
+		 
+		 
+		 int c=10;
+		 int d=20;
+		 c=c+d;
+		 System.out.println(c);
+		 c+=d;
+		 System.out.println(c);
+		 
+		 System.out.println("*********** bYte range -128 to 127 ******************");
+		 
+		 byte k=2;
+		 byte j= (byte) 130;
+		 System.out.println(j);
+		k=(byte) (k+j);
+		 //k+=j;
+		 System.out.println(k);
+		 
+		 
 	}
+	
 
 }
